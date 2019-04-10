c9edc7d6638e4e55e57e006f26524a3dba147b18
dikshamehta <500061597@stu.upes.ac.in>
2019-04-11 00:31:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/qrcode/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.hp.qrcode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView tv;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        tv = findViewById(R.id.textView2);
+        tv.setText(message);
+    }
+}
